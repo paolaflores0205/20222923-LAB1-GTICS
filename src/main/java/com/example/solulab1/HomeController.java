34359363a77489934b52a7d7725dbfb7f65e11f0
unknown <a20222923@pucp.edu.pe>
2026-01-28 14:54:56 -0500

    Se mejoro el diseño con el logo incorporado

--- src/main/java/com/example/solulab1/HomeController.java
+++ src/main/java/com/example/solulab1/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.solulab1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(name = "/")
@@ -10,7 +12,8 @@ public class HomeController {
 
     //Para el navbar
     @GetMapping("/principal")
-    public String paginaPrincipal(){
+    public String paginaPrincipal(@RequestParam(name = "code", required = false) String code, Model model){
+        model.addAttribute("codigoRecibido", code);
         return "principal"; //nombre del archivo HTML
     }
 
