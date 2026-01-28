39ec919914b0eea5f43e67161a68cae6ceb6506c
unknown <a20222923@pucp.edu.pe>
2026-01-28 12:29:37 -0500

    Se agrega la parte del navbar

---
+++ src/main/java/com/example/solulab1/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.solulab1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(name = "/")
+public class HomeController {
+
+    //Para el navbar
+    @GetMapping("/principal")
+    public String paginaPrincipal(){
+        return "principal"; //nombre del archivo HTML
+    }
+}
